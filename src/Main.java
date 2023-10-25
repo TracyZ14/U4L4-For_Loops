ae30842454fc0b56465e9fc44c4facfe575ac49c
BT_4N2_07 <TracyZ14@nycstudents.net>
2023-10-25 14:08:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,101 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        // 1a
+        for (int count = 5; count < 10; count++)
+        {
+            System.out.print(count + " ");
+        }
+        // 1b
+        for (int i = 20; i >= 0; i = i - 3)
+        {
+            System.out.print(i + " ");
+        }
+        // 1c
+        int sum = 0;
+        int x = 5;
+        for (int i = 0; i < 10 + x; i += 2)
+        {
+            sum += i;
+        }
+        System.out.println(sum);
+        */
+
+        /*
+        // 2a
+        for (int i = 5; i < 10; i++)
+        {
+            System.out.print(i + " ");
+        }
+        int i = 5;
+        while (i < 10)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+        // 2b
+        for (int i = 20; i >= 0; i = i - 3)
+        {
+            System.out.print(i + " ");
+        }
+        int i = 20;
+        while (i >= 0)
+        {
+            System.out.print(i + " ");
+            i = i - 3;
+        }
+        // 2c
+        int i = 0;
+        while (i <= 100)
+        {
+            System.out.print(i + " ");
+            i += 5;
+        }
+        for (int i = 0; i <= 100; i +=5)
+        {
+            System.out.print(i + " ");
+        }
+        // 2d
+        int i = 10;
+        int j = 40;
+        while (i <= j + 10)
+        {
+            System.out.print(i + " ");
+            i += 5;
+        }
+        int j = 40;
+        for (int i = 10; i <= j + 10; i += 5)
+        {
+            System.out.print(i + " ");
+        }
+        */
+
+        /*
+        // 3
+        for (int i = 10; i < 5; i++)
+        {
+            System.out.print(i + " ");
+        }
+        */
+
+        /*
+        // 4
+        for (int i = 5; i > 0; i++)
+        {
+            System.out.println(i);
+        }
+        */
+
+        /*
+        // 5
+        for (int i = 4; i <= 11; i++)
+        {
+            System.out.print(i + " ");
+        }
+        for (int i = 0; i <= 7; i++)
+        {
+            System.out.print((i + 4) + " ");
+        }
+        */
+    }
+}
